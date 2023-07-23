ad630d85577a9f7f35cc679c43753ae085b67ef2
PhamNhuQuocBao <phamnhuquocbao@gmail.com>
2023-07-23 16:14:31 +0700

    feat/4-create-form

---
+++ src/constants/form.js
@@ -0,0 +1,22 @@
+const INPUT_FIELD = [
+  {
+    label: 'Fullname',
+    inputType: 'text',
+    name: 'name',
+    id: 'name',
+  },
+  {
+    label: 'Age',
+    inputType: 'number',
+    name: 'age',
+    id: 'age',
+  },
+  {
+    label: 'Address',
+    inputType: 'text',
+    name: 'address',
+    id: 'address',
+  },
+];
+
+export default INPUT_FIELD;
